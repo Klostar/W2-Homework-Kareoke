5181332e5c5aa8938f3bcfc461841614696a0f7d
chloe alcorn <ca.alcorn@outlook.com>
2018-08-10 19:38:42 +0100

    updated require file names to singular and added customer wallet + tested

--- guest.rb
+++ guest.rb
@@ -1,9 +1,11 @@
 class Guest
 
 attr_reader(:guest_name)
+attr_accessor(:wallet)
 
-  def initialize(guest_name)
+  def initialize(guest_name,wallet)
     @guest_name = guest_name
+    @wallet = wallet
   end
 
 end
--- specs/guests_spec.rb
+++ specs/guests_spec.rb
@@ -1,19 +1,23 @@
 require("minitest/autorun")
 require("minitest/rg")
-require_relative("../guests.rb")
-require_relative("../songs.rb")
+require_relative("../guest.rb")
+require_relative("../song.rb")
 
 class GuestTest < MiniTest::Test
 
   def setup()
-    @guest1 = Guest.new("Ryan")
-    @guest2 = Guest.new("Dan")
-    @guest3 = Guest.new("Katie")
-    @guest4 = Guest.new("Amy")
+    @guest1 = Guest.new("Ryan", 55)
+    @guest2 = Guest.new("Dan", 40)
+    @guest3 = Guest.new("Katie", 45)
+    @guest4 = Guest.new("Amy", 50)
   end
 
   def test_guest_has_name
     assert_equal("Amy", @guest4.guest_name)
+  end
+
+  def test_customer_has_money
+assert_equal(55, @guest1.wallet)
   end
 
 end
